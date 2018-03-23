9d82fd62af1a6c468c55540037dbcdbbbf196250
Mark Sellers <mark@masenterprisesinc.com;msellers@ba.org;msellers@onechildmatters.org>
2018-03-22 19:05:01 -0600

    New specs and changes for the OCM customized Transfer/Reassign & Reactivate forms.

---
+++ MoM.Sponsorship.UIModel/AddIns/OCMSponsorshipTransferReassign.AddIn.vb
@@ -0,0 +1,11 @@
+Partial Public NotInheritable Class OCMSponsorshipTransferReassignAddIn
+
+    Private Sub OnInit()
+        'This method is called when the UI model is created to allow any initialization to be performed.
+        TAB_EXPIRATION.Visible = False
+    End Sub
+
+
+
+End Class
+
