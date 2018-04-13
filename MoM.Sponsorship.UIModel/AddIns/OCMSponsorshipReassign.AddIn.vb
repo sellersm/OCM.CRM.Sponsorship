81d9eca5f839d7558694d4990a8d8002a12ef181
Mark Sellers <mark@masenterprisesinc.com;msellers@ba.org;msellers@onechildmatters.org>
2018-04-12 19:09:31 -0600

    Latest changes for the new OCM customized forms for Reactivate, Reassign, Transfer of sponsorships.

---
+++ MoM.Sponsorship.UIModel/AddIns/OCMSponsorshipReassign.AddIn.vb
@@ -0,0 +1,9 @@
+Partial Public NotInheritable Class OCMSponsorshipReassignAddIn
+
+    Private Sub OnInit()
+        'This method is called when the UI model is created to allow any initialization to be performed.
+        TAB_EXPIRATION.Visible = False
+    End Sub
+
+End Class
+
